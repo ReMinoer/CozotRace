b1717fec0a304ed3797fd5f6eea1fbdc4b96186e
Ewakhine <wenell@hotmail.fr>
2015-01-28 21:38:41 +0100

    Demo unity
    Demo scene with basics options.

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour {
+
+	public float playerSpeed = 4.0f;
+
+	void Start () {
+	
+		//Player spawn point
+		transform.position = new Vector3 (0.0f, 0.05f, 4.0f);
+	}
+
+	void Update () {
+	
+		Vector3 vert = new Vector3 (0.0f, 0.0f, -1.0f);
+		Vector3 rotate = new Vector3(0.0f,100.0f,0.0f);
+		transform.Rotate(rotate * Input.GetAxis ("Horizontal") * Mathf.Ceil(Input.GetAxis ("Vertical")) * playerSpeed/2.0f * Time.deltaTime, Space.World);
+		transform.Translate (vert * Input.GetAxis ("Vertical") * playerSpeed * Time.deltaTime);
+	}
+}
