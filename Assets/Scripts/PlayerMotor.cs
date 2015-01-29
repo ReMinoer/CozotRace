75810b02b32c7eef5b6b7986e498a7078d1d124a
Ewakhine <wenell@hotmail.fr>
2015-01-29 10:36:22 +0100

    Friday's Demo extended
    Several corrections about script and presentation's scene.

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -2,20 +2,16 @@
 using System.Collections;
 
 public class PlayerMotor : MonoBehaviour {
-
-	public float playerSpeed = 4.0f;
+	
+	public float PlayerSpeed = 4.0f;
+	public float TurnSpeed = 2.0f;
+	public float TurnPower = 100.0f;
 
 	void Start () {
-	
-		//Player spawn point
-		transform.position = new Vector3 (0.0f, 0.05f, 4.0f);
 	}
 
 	void Update () {
-	
-		Vector3 vert = new Vector3 (0.0f, 0.0f, -1.0f);
-		Vector3 rotate = new Vector3(0.0f,100.0f,0.0f);
-		transform.Rotate(rotate * Input.GetAxis ("Horizontal") * Mathf.Ceil(Input.GetAxis ("Vertical")) * playerSpeed/2.0f * Time.deltaTime, Space.World);
-		transform.Translate (vert * Input.GetAxis ("Vertical") * playerSpeed * Time.deltaTime);
+		transform.Rotate(TurnPower * Vector3.up * Input.GetAxis ("Horizontal") * Mathf.Ceil(Input.GetAxis ("Vertical")) * TurnSpeed * Time.deltaTime, Space.World);
+		transform.Translate (Vector3.back * Input.GetAxis ("Vertical") * PlayerSpeed * Time.deltaTime);
 	}
 }
