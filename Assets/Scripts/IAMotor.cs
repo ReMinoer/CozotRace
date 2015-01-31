94cfe69baf84fb7c72cd8b2ab14f21b17ec2b490
ReMinoer <rmorillon@freneticexperience.com>
2015-01-31 21:58:04 +0100

    Simple route follow

---
+++ Assets/Scripts/IAMotor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IAMotor : MonoBehaviour
+{
+	public float Speed = 5;
+
+	private GameObject _lastPoint;
+	private GameObject _nextPoint;
+
+	void Start ()
+	{
+		_lastPoint = Track.Instance.StartPoint;
+		_nextPoint = _lastPoint.GetComponent<TrackPoint>().NextPoint;
+	}
+
+	void Update ()
+	{
+		Vector3 destination = _nextPoint.transform.position;
+		Vector3 direction = (destination - this.transform.position).normalized;
+		Vector3 diff = destination - _lastPoint.transform.position;
+
+		this.transform.position += direction * Speed * Time.deltaTime;
+
+		if (destination == this.transform.position || Vector3.Dot(direction, diff) < 0)
+		{
+			this.transform.position = destination;
+			_lastPoint = _nextPoint;
+			if (_nextPoint.GetComponent<TrackPoint>().NextPoint != null)
+				_nextPoint = _nextPoint.GetComponent<TrackPoint>().NextPoint;
+			else if (_lastPoint.transform.position == Track.Instance.StartPoint.transform.position)
+				_nextPoint = Track.Instance.StartPoint.GetComponent<TrackPoint>().NextPoint;
+			else
+				_nextPoint = Track.Instance.StartPoint;
+		}
+	}
+}
