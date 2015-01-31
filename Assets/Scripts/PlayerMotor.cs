82ef22199691e871b62d517997c32c0492244e71
Ewakhine <wenell@hotmail.fr>
2015-01-31 23:31:58 +0100

    Add road's texture's influence

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,12 +6,25 @@ public class PlayerMotor : MonoBehaviour {
 	public float PlayerSpeed = 4.0f;
 	public float TurnSpeed = 2.0f;
 	public float TurnPower = 100.0f;
+	public Texture asphalt;
+	public Texture grass;
+
+	private float ReducerSpeed = 1.0f;
 
 	void Start () {
 	}
 
 	void Update () {
-		transform.Rotate(TurnPower * Vector3.up * Input.GetAxis ("Horizontal") * Mathf.Ceil(Input.GetAxis ("Vertical")) * TurnSpeed * Time.deltaTime, Space.World);
-		transform.Translate (Vector3.back * Input.GetAxis ("Vertical") * PlayerSpeed * Time.deltaTime);
+		transform.Rotate(ReducerSpeed *TurnPower * Vector3.up * Input.GetAxis ("Horizontal") * Mathf.Ceil(Input.GetAxis ("Vertical")) * TurnSpeed * Time.deltaTime, Space.World);
+		transform.Translate (ReducerSpeed *Vector3.back * Input.GetAxis ("Vertical") * PlayerSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerStay (Collider collider) {
+		if (collider.gameObject.renderer.material.mainTexture == grass) {
+			ReducerSpeed = 2.0f;
+		} 
+		else if (collider.gameObject.renderer.material.mainTexture == asphalt) {
+			ReducerSpeed = 0.5f;
+		}
 	}
 }
