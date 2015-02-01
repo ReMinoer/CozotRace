a163b3120a89597c488e6e75c70d84935ecc0002
Ewakhine <wenell@hotmail.fr>
2015-02-01 12:28:37 +0100

    Modification of PlayerMotorTexture's struct GroundProperty

--- Assets/Scripts/PlayerMotorTexture.cs
+++ Assets/Scripts/PlayerMotorTexture.cs
@@ -1,34 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerMotorTexture : MonoBehaviour {
 
 	public struct GroundProperty {
-		public Texture texture;
-		public float ReducerSpeedTexture;
+		public Texture FloorTexture;
+		public float MultiplierSpeedTexture;
 	}
 
 	public float PlayerSpeed = 4.0f;
 	public float TurnSpeed = 2.0f;
 	public float TurnPower = 100.0f;
-	public GroundProperty[] floor;
 	
-	private float ReducerSpeed = 1.0f;
+	public Texture FloorAcceleratorTexture;
+	public float FloorAcceleratorSpeed = 2.0f;
+	public Texture FloorReducerTexture;
+	public float FloorReducerSpeed = 0.5f;
+	
+	private List<GroundProperty> Floor = new List<GroundProperty>();
+	private float MultiplierSpeed = 1.0f;
 	
 	void Start () {
+		GroundProperty FloorAccelerator = new GroundProperty();
+		FloorAccelerator.FloorTexture = FloorAcceleratorTexture;
+		FloorAccelerator.MultiplierSpeedTexture = FloorAcceleratorSpeed;
+
+		GroundProperty FloorReducer = new GroundProperty();
+		FloorReducer.FloorTexture = FloorReducerTexture;
+		FloorReducer.MultiplierSpeedTexture = FloorReducerSpeed;
+
+		Floor.Add (FloorAccelerator);
+		Floor.Add (FloorReducer);
 	}
 	
 	void Update () {
 		transform.Rotate(TurnPower * Vector3.up * Input.GetAxis ("Horizontal") * Mathf.Ceil(Input.GetAxis ("Vertical")) * TurnSpeed * Time.deltaTime, Space.World);
-		transform.Translate (ReducerSpeed *Vector3.back * Input.GetAxis ("Vertical") * PlayerSpeed * Time.deltaTime);
+		transform.Translate (MultiplierSpeed *Vector3.back * Input.GetAxis ("Vertical") * PlayerSpeed * Time.deltaTime);
 	}
 	
 	void OnTriggerStay (Collider collider) {
-		if (collider.gameObject.renderer.material.mainTexture == floor[0].texture) {
-			ReducerSpeed = floor[0].ReducerSpeedTexture;
-		} 
-		else if (collider.gameObject.renderer.material.mainTexture == floor[1].texture) {
-			ReducerSpeed = floor[1].ReducerSpeedTexture;
-		}
+		Floor.ForEach (delegate(GroundProperty floor) {
+				if (collider.gameObject.renderer.material.mainTexture == floor.FloorTexture) {
+						MultiplierSpeed = floor.MultiplierSpeedTexture;
+				}
+		});
 	}
 }
