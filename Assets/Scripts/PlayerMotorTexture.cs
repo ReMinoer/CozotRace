4d5f0c288115965eb68332459ed74a8863acc21a
Ewakhine <wenell@hotmail.fr>
2015-02-01 11:24:01 +0100

    PlayerMotorTexture's struct GroundProperty

--- Assets/Scripts/PlayerMotorTexture.cs
+++ Assets/Scripts/PlayerMotorTexture.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class PlayerMotorTexture : MonoBehaviour {
-	
+
+	public struct GroundProperty {
+		public Texture texture;
+		public float ReducerSpeedTexture;
+	}
+
 	public float PlayerSpeed = 4.0f;
 	public float TurnSpeed = 2.0f;
 	public float TurnPower = 100.0f;
-	public Texture asphalt;
-	public float ReducerSpeedTexture1 = 2.0f;
-	public Texture grass;
-	public float ReducerSpeedTexture2 = 0.8f;
+	public GroundProperty[] floor;
 	
 	private float ReducerSpeed = 1.0f;
 	
@@ -17,16 +19,16 @@ public class PlayerMotorTexture : MonoBehaviour {
 	}
 	
 	void Update () {
-		transform.Rotate(ReducerSpeed *TurnPower * Vector3.up * Input.GetAxis ("Horizontal") * Mathf.Ceil(Input.GetAxis ("Vertical")) * TurnSpeed * Time.deltaTime, Space.World);
+		transform.Rotate(TurnPower * Vector3.up * Input.GetAxis ("Horizontal") * Mathf.Ceil(Input.GetAxis ("Vertical")) * TurnSpeed * Time.deltaTime, Space.World);
 		transform.Translate (ReducerSpeed *Vector3.back * Input.GetAxis ("Vertical") * PlayerSpeed * Time.deltaTime);
 	}
 	
 	void OnTriggerStay (Collider collider) {
-		if (collider.gameObject.renderer.material.mainTexture == grass) {
-			ReducerSpeed = ReducerSpeedTexture1;
+		if (collider.gameObject.renderer.material.mainTexture == floor[0].texture) {
+			ReducerSpeed = floor[0].ReducerSpeedTexture;
 		} 
-		else if (collider.gameObject.renderer.material.mainTexture == asphalt) {
-			ReducerSpeed = ReducerSpeedTexture2;
+		else if (collider.gameObject.renderer.material.mainTexture == floor[1].texture) {
+			ReducerSpeed = floor[1].ReducerSpeedTexture;
 		}
 	}
 }
