ee626d64d4ddf20a7fdd8f48d3871afa4c9707a0
Ewakhine <wenell@hotmail.fr>
2015-02-02 09:18:12 +0100

    GroundDetector's script

---
+++ Assets/Scripts/Vehicle/GroundDetector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundDetector : MonoBehaviour {
+
+	public PlayerMotorTexture vehicle;
+
+	void Start () {
+	}
+
+	void Update () {
+	}
+
+	void OnTriggerStay (Collider collider)
+	{
+		foreach (Map.GroundProperty ground in Map.Instance.Grounds)
+			if (collider.gameObject.renderer.material.mainTexture == ground.Texture)
+		{
+			vehicle.SpeedCoeff = ground.SpeedCoeff;
+			break;
+		}
+	}
+}
--- Assets/Scripts/Vehicle/PlayerMotorTexture.cs
+++ Assets/Scripts/Vehicle/PlayerMotorTexture.cs
@@ -7,6 +7,7 @@ public class PlayerMotorTexture : MonoBehaviour
 	public float Speed = 6.0f;
 	public float TurnSpeed = 200.0f;
 
+	public float SpeedCoeff { set { _speedCoeff = value; } }
 	private float _speedCoeff = 1.0f;
 
 	void Start()
@@ -19,15 +20,5 @@ public class PlayerMotorTexture : MonoBehaviour
 		float verticalInput = Input.GetAxis("Vertical");
 		transform.Rotate(horizontalInput * Mathf.Ceil(verticalInput) * TurnSpeed * Vector3.up * Time.deltaTime, Space.World);
 		transform.Translate (verticalInput * _speedCoeff * Speed * Vector3.back * Time.deltaTime);
-	}
-	
-	void OnTriggerStay (Collider collider)
-	{
-		foreach (Map.GroundProperty ground in Map.Instance.Grounds)
-			if (collider.gameObject.renderer.material.mainTexture == ground.Texture)
-			{
-				_speedCoeff = ground.SpeedCoeff;
-				break;
-			}
 	}
 }
