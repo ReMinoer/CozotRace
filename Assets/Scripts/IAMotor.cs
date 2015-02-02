00411f9a043e1d1dfdad8d8b4d346db5a374711a
ReMinoer <rmorillon@freneticexperience.com>
2015-02-02 22:48:17 +0100

    AI smooth turn prototype

--- Assets/Scripts/IAMotor.cs
+++ Assets/Scripts/IAMotor.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class IAMotor : MonoBehaviour
 {
-	public float Speed = 5;
+	public float Speed = 2;
+	public float TurnSpeed = 2;
 
 	private GameObject _lastPoint;
 	private GameObject _nextPoint;
@@ -16,12 +17,46 @@ public class IAMotor : MonoBehaviour
 
 	void Update ()
 	{
+		//SimpleFollow();
+		SmoothTurnFollow();
+	}
+
+	void SmoothTurnFollow()
+	{
 		Vector3 destination = _nextPoint.transform.position;
 		Vector3 direction = (destination - this.transform.position).normalized;
 		Vector3 diff = destination - _lastPoint.transform.position;
+		
+		Vector3 orientation = (this.transform.right).normalized;
+		
+		float angle = Vector3.Angle(direction, orientation);
+		Vector3 cross = Vector3.Cross(direction, orientation);
+		float turn = Mathf.Min(angle, TurnSpeed);
+		this.transform.Rotate(this.transform.up, cross.y > 0 ? -turn : turn);
+		orientation = (this.transform.right).normalized;
+		
+		this.transform.position += orientation * Speed * Time.deltaTime;
+		
+		if (destination == this.transform.position || Vector3.Dot(direction, diff) < 0)
+		{
+			_lastPoint = _nextPoint;
+			if (_nextPoint.GetComponent<TrackPoint>().NextPoint != null)
+				_nextPoint = _nextPoint.GetComponent<TrackPoint>().NextPoint;
+			else if (_lastPoint.transform.position == Track.Instance.StartPoint.transform.position)
+				_nextPoint = Track.Instance.StartPoint.GetComponent<TrackPoint>().NextPoint;
+			else
+				_nextPoint = Track.Instance.StartPoint;
+		}
+	}
 
+	void SimpleFollow()
+	{
+		Vector3 destination = _nextPoint.transform.position;
+		Vector3 direction = (destination - this.transform.position).normalized;
+		Vector3 diff = destination - _lastPoint.transform.position;
+		
 		this.transform.position += direction * Speed * Time.deltaTime;
-
+		
 		if (destination == this.transform.position || Vector3.Dot(direction, diff) < 0)
 		{
 			this.transform.position = destination;
