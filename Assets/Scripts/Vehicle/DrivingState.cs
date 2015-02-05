b12da80844f58fefd6ef20c1f59d7f93c951fb9e
ReMinoer <rmorillon@freneticexperience.com>
2015-02-05 19:53:53 +0100

    Create DrivingState

---
+++ Assets/Scripts/Vehicle/DrivingState.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class DrivingState
+{
+	// Continous state
+	public float Accelerate; // [0;1]
+	public float Brake; // [0;1] (include backward)
+	public float Turn; // [-TurnSpeedMax;TurnSpeedMax]
+	public bool Boost;
+
+	// Ponctual state
+	public bool DashLeft;
+	public bool DashRight;
+
+	// Unsynchronize fix
+	public Vector3 Position;
+	public Quaternion Rotation;
+
+	public bool HasChange(DrivingState other)
+	{
+		return Accelerate != other.Accelerate
+			|| Brake != other.Brake
+			|| Turn != other.Turn
+			|| Boost != other.Boost
+			|| DashLeft
+			|| DashRight;
+	}
+}
