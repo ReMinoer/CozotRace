c74f9231e96ac186f553deeade8356a1e6097c8b
KorriZokta <frederic.becker4@gmail.com>
2015-02-06 11:17:45 +0100

    VehicleMotor, PlayerMotor and ProtoVehicle

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -2,16 +2,140 @@
 using System.Collections;
 
 public class PlayerMotor : MonoBehaviour {
-	
-	public float PlayerSpeed = 4.0f;
-	public float TurnSpeed = 2.0f;
-	public float TurnPower = 100.0f;
-
+	public float ForwardAccelerationForce=2.5f;
+	public float BackwardAccelerationForce=2.5f;
+	public float BrakeForce=3f;
+	public float Speed = 0f;
+	public float ForwardSpeedLimit = 25f;
+	public float BackwardSpeedLimit = 10f;
+	public bool GoForward = false;
+	public bool GoBackward = false;
+	// Use this for initialization
 	void Start () {
+	
 	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Speed = rigidbody.velocity.magnitude;
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis ("Vertical");
 
-	void Update () {
-		transform.Rotate(TurnPower * Vector3.up * Input.GetAxis ("Horizontal") * Mathf.Ceil(Input.GetAxis ("Vertical")) * TurnSpeed * Time.deltaTime, Space.World);
-		transform.Translate (Vector3.back * Input.GetAxis ("Vertical") * PlayerSpeed * Time.deltaTime);
+		if (Speed < 1 && Speed > 0) {
+			if(verticalInput > 0) {
+				if(!GoForward) { 
+					rigidbody.velocity = rigidbody.velocity * 0;
+				}
+				else {
+					GetComponent<VehicleMotor>().ForwardAcceleration(-10*ForwardAccelerationForce * transform.forward * Time.deltaTime);
+					if(rigidbody.velocity.magnitude > ForwardSpeedLimit)
+						rigidbody.velocity = rigidbody.velocity.normalized*ForwardSpeedLimit;
+				}
+			}
+			if(verticalInput<0) {
+				if(GoForward) {
+					rigidbody.velocity = rigidbody.velocity * 0;
+				}
+				else {
+					GetComponent<VehicleMotor>().BackwardAcceleration(10*BackwardAccelerationForce * transform.forward * Time.deltaTime);
+					if(rigidbody.velocity.magnitude > BackwardSpeedLimit)
+						rigidbody.velocity = rigidbody.velocity.normalized*BackwardSpeedLimit;
+				}
+			}
+		}
+		if(Speed>=1) {
+			if(verticalInput>0) {
+				if(!GoForward) {
+					GetComponent<VehicleMotor>().Brake (-10*BrakeForce * transform.forward * Time.deltaTime);
+				}
+				else {
+					GetComponent<VehicleMotor>().ForwardAcceleration(-10*ForwardAccelerationForce * transform.forward * Time.deltaTime);
+					if(rigidbody.velocity.magnitude > ForwardSpeedLimit)
+						rigidbody.velocity = rigidbody.velocity.normalized*ForwardSpeedLimit;
+				}
+			}
+			if(verticalInput<0) {
+				if(GoForward) {
+					GetComponent<VehicleMotor>().Brake (10*BrakeForce * transform.forward * Time.deltaTime);
+				}
+				else {
+					GetComponent<VehicleMotor>().BackwardAcceleration(10*BackwardAccelerationForce * transform.forward * Time.deltaTime);
+					if(rigidbody.velocity.magnitude > BackwardSpeedLimit)
+						rigidbody.velocity = rigidbody.velocity.normalized*BackwardSpeedLimit;
+				}
+			}
+		}
+		if(Speed==0) {
+			if(verticalInput>0) {
+				GoForward=true;
+				GetComponent<VehicleMotor>().ForwardAcceleration(-10*ForwardAccelerationForce * transform.forward * Time.deltaTime);
+				if(rigidbody.velocity.magnitude > ForwardSpeedLimit)
+					rigidbody.velocity = rigidbody.velocity.normalized*ForwardSpeedLimit;
+			}
+			if(verticalInput<0) {
+				GoForward=false;
+				GetComponent<VehicleMotor>().BackwardAcceleration(10*BackwardAccelerationForce * transform.forward * Time.deltaTime);
+				if(rigidbody.velocity.magnitude > BackwardSpeedLimit)
+					rigidbody.velocity = rigidbody.velocity.normalized*BackwardSpeedLimit;
+			}
+		}
+
+
+		/*if (GoForward) {
+			if (verticalInput > 0) {
+				GoForward=true;
+				GetComponent<VehicleMotor>().ForwardAcceleration(-10*ForwardAccelerationForce * transform.forward * Time.deltaTime);
+				if(rigidbody.velocity.magnitude > ForwardSpeedLimit)
+					rigidbody.velocity = rigidbody.velocity.normalized*ForwardSpeedLimit;
+			}
+			if (verticalInput < 0) {
+				if (1 > rigidbody.velocity.magnitude && rigidbody.velocity.magnitude > 0) {
+					GoForward=true;
+					rigidbody.velocity = rigidbody.velocity*0;
+					rigidbody.Sleep();
+				}
+				if (rigidbody.velocity.magnitude == 0) {
+					GoForward=false;
+					GetComponent<VehicleMotor>().BackwardAcceleration(10*BackwardAccelerationForce * transform.forward * Time.deltaTime);
+					if(rigidbody.velocity.magnitude > BackwardSpeedLimit)
+						rigidbody.velocity = rigidbody.velocity.normalized*BackwardSpeedLimit;
+				}
+				else {
+					GoForward=true;
+					GetComponent<VehicleMotor>().Brake (10*BrakeForce * transform.forward * Time.deltaTime);
+				}
+			}
+		} 
+		else {
+			if (verticalInput > 0) {
+				if (1 > rigidbody.velocity.magnitude && rigidbody.velocity.magnitude > 0) {
+					GoForward=false;
+					rigidbody.velocity = rigidbody.velocity*0;
+					rigidbody.Sleep();
+				}
+				else if(rigidbody.velocity.magnitude == 0) {
+					GoForward=true;
+					GetComponent<VehicleMotor>().ForwardAcceleration(-10*ForwardAccelerationForce * transform.forward * Time.deltaTime);
+					if(rigidbody.velocity.magnitude > ForwardSpeedLimit)
+						rigidbody.velocity = rigidbody.velocity.normalized*ForwardSpeedLimit;
+				}
+				else {
+					GoForward=false;
+					GetComponent<VehicleMotor>().Brake (-10*BrakeForce * transform.forward * Time.deltaTime);
+				}
+			}
+			if (verticalInput < 0) {
+				GoForward=false;
+				GetComponent<VehicleMotor>().BackwardAcceleration(10*BackwardAccelerationForce * transform.forward * Time.deltaTime);
+				if(rigidbody.velocity.magnitude > BackwardSpeedLimit)
+					rigidbody.velocity = rigidbody.velocity.normalized*BackwardSpeedLimit;
+			}
+		}*/
+		if (horizontalInput < 0) {
+			GetComponent<VehicleMotor> ().LeftRotation ();
+		}
+		if (horizontalInput > 0) {
+			GetComponent<VehicleMotor> ().RightRotation ();
+		}
 	}
 }
---
+++ Assets/Scripts/VehicleMotor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleMotor : MonoBehaviour {
+
+	public void ForwardAcceleration(Vector3 forwardAccelerationForce) {
+		rigidbody.AddForce(forwardAccelerationForce,ForceMode.Impulse);
+	}
+
+	public void Brake(Vector3 brakeForce) {
+		rigidbody.AddForce(brakeForce,ForceMode.Impulse);
+	}
+
+	public void BackwardAcceleration(Vector3 backwardAccelerationForce) {
+		rigidbody.AddForce(backwardAccelerationForce,ForceMode.Impulse);
+	}
+
+	public void LeftRotation() {
+		Vector3 angle = Vector3.down*100*Time.deltaTime;
+		transform.Rotate (angle);
+		}
+
+	public void RightRotation() {
+		Vector3 angle = Vector3.up*100*Time.deltaTime;
+		transform.Rotate (angle);
+		}
+}
