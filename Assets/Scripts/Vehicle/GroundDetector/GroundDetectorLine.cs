9c9bc8cae942def9471dfb7e22f09f1b74150f71
Rémi Morillon <remi_morillon@hotmail.fr>
2015-02-12 19:17:00 +0100

    Add GroundDetectorLine and update GroundDetectorSystem

---
+++ Assets/Scripts/Vehicle/GroundDetector/GroundDetectorLine.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using DesignPattern;
+using System.Collections;
+using System.Linq;
+
+public class GroundDetectorLine : Factory<GroundDetectorLine>
+{
+	public int Count = 5;
+	public float Interval = 1;
+	public float DetectorsHeight = 2;
+	public bool IgnoreFirst = false;
+
+	public Vector3 Destination
+	{
+		get
+		{
+			return Detectors.Last().transform.position;
+		}
+		set
+		{
+			Interval = (value - transform.position).magnitude / (Count + (IgnoreFirst ? 1 : 0));
+		}
+	}
+
+	public GroundDetectorV2[] Detectors { get; private set; }
+
+	void Start ()
+	{
+		Detectors = new GroundDetectorV2[Count];
+		
+		for (int i = 0; i < Count; i++)
+		{
+			Detectors[i] = GroundDetectorV2.New("GroundDetector");
+			
+			Detectors[i].transform.parent = transform;
+			Detectors[i].transform.localPosition = -transform.forward * ((i + (IgnoreFirst ? 1 : 0)) * Interval);
+			
+			var boxCollider = Detectors[i].collider as BoxCollider;
+			if (boxCollider != null)
+			{
+				Vector3 size = boxCollider.size;
+				boxCollider.size = new Vector3(size.x, DetectorsHeight, size.z);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Vehicle/GroundDetector/GroundDetectorSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class GroundDetectorSystem : MonoBehaviour
+{
+    public int LateralCount = 5;
+	public float Interval = 1;
+	public float Angle = 15;
+    public float DetectorsHeight = 2;
+
+	public GroundDetectorV2 Central { get; private set; }
+	public GroundDetectorLine Left { get; private set; }
+	public GroundDetectorLine Right { get; private set; }
+
+	void Awake()
+	{
+        Central = GroundDetectorV2.New("GroundDetector");
+        Central.transform.parent = transform;
+
+		Left = GroundDetectorLine.New();
+		Left.Count = LateralCount;
+		Left.Interval = Interval;
+		Left.DetectorsHeight = DetectorsHeight;
+		Left.IgnoreFirst = true;
+
+		Left.transform.parent = transform;
+		Left.transform.rotation = Quaternion.AngleAxis(-Angle, transform.up);
+		
+		Right = GroundDetectorLine.New();
+		Right.Count = LateralCount;
+		Right.Interval = Interval;
+		Right.DetectorsHeight = DetectorsHeight;
+		Right.IgnoreFirst = true;
+
+		Right.transform.parent = transform;
+		Right.transform.rotation = Quaternion.AngleAxis(Angle, transform.up);
+	}
+}
