5b9a0a526eaa88784b19d09f23300b4e2700e601
KorriZokta <frederic.becker4@gmail.com>
2015-05-02 17:30:49 +0200

    Add ParticleBehaviour (it works !)

---
+++ Assets/Scripts/Vehicle/ReactorBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactorBehaviour : MonoBehaviour {
+
+	public GameObject Vehicle;
+
+	// Use this for initialization
+	void Start () {
+		Vehicle.GetComponent<VehicleMotor>().StateChanged += DrivingStateChanged;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void DrivingStateChanged(object sender, VehicleMotor.StateChangedEventArgs e) {
+		ParticleSystem syst = GetComponent<ParticleSystem> ();
+		if (syst != null) {
+						if (e.State.Forward > 0)
+								syst.Play ();
+						else
+								syst.Stop ();
+						
+				}
+}
+}
