f05027109609d98de6db5909a7827298c8660a26
ReMinoer <rmorillon@freneticexperience.com>
2015-05-11 20:23:11 +0200

    Add StartGrid prefab and update GameManager prefab

--- Assets/Scripts/Race/StartGrid.cs
+++ Assets/Scripts/Race/StartGrid.cs
@@ -1,18 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class StartGrid : MonoBehaviour {
-	public int Columns;
-	public int LineInterval;
-	public int LateralInterval;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class StartGrid : MonoBehaviour
+{
+    public Transform[] StartPositions;
 }
