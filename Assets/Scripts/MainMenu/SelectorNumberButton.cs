272b957e6d656d8ebe3ed5f4943599b0d21f4eb8
Ewakhine <wenell@hotmail.fr>
2015-05-23 21:43:50 +0200

    ProtoSelector2 with selection of maps, number of players and vehicle (not implemented).

---
+++ Assets/Scripts/MainMenu/SelectorNumberButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SelectorNumberButton : MonoBehaviour {
+
+	public Text _numberText;
+	
+	public void NextSelectionNumber() {
+		int nextInt = int.Parse(_numberText.text) + 1;
+		_numberText.text = "" + nextInt;
+
+		if (int.Parse (_numberText.text) > 8)
+			_numberText.text = "8";
+	}
+	
+	public void PreviousSelectionNumber() {
+		int previousInt = int.Parse(_numberText.text) - 1;
+		_numberText.text = "" + previousInt;
+		if (int.Parse (_numberText.text) < 2)
+			_numberText.text = "2";
+	}
+}
