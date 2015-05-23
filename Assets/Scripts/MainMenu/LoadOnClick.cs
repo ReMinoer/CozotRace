118336623ef565f5b64007cf6bf8656e4624bb6c
Ewakhine <wenell@hotmail.fr>
2015-05-23 18:58:47 +0200

    Changes in ProtoSelector : add sprites and change LoadOnClick's script to adapt TitleRace Text with loadingScene.

--- Assets/Scripts/MainMenu/LoadOnClick.cs
+++ Assets/Scripts/MainMenu/LoadOnClick.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LoadOnClick : MonoBehaviour {
 
-	public GameObject loadingImage;
+	public GameObject _loadingImage;
+	public Text _text;
+
+	private string[] newText = {"", "EasyPlain", "GrassHills", "SpringLoop"};
 
 	public void LoadScene(int level) 
 	{
-		loadingImage.SetActive (true);
+		_loadingImage.SetActive (true);
 		Application.LoadLevel (level);
+		_text.text = newText[level-1];
 	}
 }
