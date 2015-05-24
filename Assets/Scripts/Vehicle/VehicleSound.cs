8086d8b3ef7df320bdd395d52e4dd751493acff5
Maxime593 <broy.maxime@gmail.com>
2015-05-24 18:13:13 +0200

    Add ProtoSound

---
+++ Assets/Scripts/Vehicle/VehicleSound.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class VehicleSound : MonoBehaviour {
+    public AudioSource motorSound;
+    float lastForward;
+	// Use this for initialization
+	void Start () {
+        lastForward = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Awake()
+    {
+        GetComponent<VehicleMotor>().StateChanged += OnVehicleStateChanged;
+    }
+    public void OnVehicleStateChanged(object sender, VehicleMotor.StateChangedEventArgs args)
+    {
+        
+        // Traitement en fonction de args.State
+        if(args.State.Forward > 0.0 && lastForward == 0)
+        {
+            motorSound.Play();
+        }
+        lastForward = args.State.Forward;
+        //if (GetComponent<VehicleMotor>().SpeedEvolution == 0)
+        if(args.State.Forward == 0)
+        {
+            motorSound.Stop();
+        }
+    }
+}
