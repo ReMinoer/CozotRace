c170d2b2ba67a89be736aaed976f4705aa23ebda
Ewakhine <wenell@hotmail.fr>
2015-05-26 20:53:11 +0200

    First finished version of ConfigMenuManager.

--- Assets/Scripts/MainMenu/ConfigMenuManager.cs
+++ Assets/Scripts/MainMenu/ConfigMenuManager.cs
@@ -4,22 +4,48 @@ using UnityEngine.UI;
 
 public class ConfigMenuManager : MonoBehaviour {
 
-	public GameObject[] PanelList;
-
+	public RectTransform RaceControler;
+	public Image RaceChoiceImage;
+	public Sprite[] RaceImages;
+	public Text RaceName;
 	public string[] mapNames = {"EasyPlain", "GrassHills", "SpringLoop"};
-	public string[] vehiclesName;
+
+	public RectTransform VehicleControler;
+	public Image VehicleChoiceImage;
+	public Sprite[] VehicleImages;
+	public Text VehicleName;
+	public string[] vehicleNames;
+
+	public RectTransform NumberPlayerControler;
+	public Text CounterPlayers;
 	public int[] numberPlayers = {1, 2, 3, 4};
+
+	
+	public RectTransform NumberVehicleControler;
+	public Text CounterVehicles;
 	public int[] numberVehicles = {2, 3, 4, 5, 6, 7 ,8};
 
-	void start() {
-		Debug.Log ("Piou");
-	}
+	public RectTransform NumberTurnControler;
+	public Text CounterTurn;
+	public int[] numberTurns = {1, 2, 3, 4};
 
-	void update() {
-		Debug.Log("piou");
-		for (int i= 0; i < PanelList.Length; i++) 
-		{
-			PanelList[i].GetComponentInChildren<Text>().text = "" + PanelList[i].GetComponentInChildren<Controler>().value;
-		}
+	void Update() {
+		int _raceValue = RaceControler.GetComponent<Controler>().value;
+		RaceChoiceImage.sprite = RaceImages [_raceValue];
+		RaceName.text = mapNames [_raceValue];
+		
+		int _vehicleValue = VehicleControler.GetComponent<Controler> ().value;
+		VehicleChoiceImage.sprite = VehicleImages [_vehicleValue];
+		VehicleName.text = vehicleNames [_vehicleValue];
+
+		int _playerValue = NumberPlayerControler.GetComponent<Controler> ().value;
+		CounterPlayers.text = "" + numberPlayers [_playerValue];
+		
+		int _numberVehicleValue = NumberVehicleControler.GetComponent<Controler> ().value;
+		CounterVehicles.text = "" + numberVehicles [_numberVehicleValue];
+		
+		int _numberTurnValue = NumberTurnControler.GetComponent<Controler> ().value;
+		CounterTurn.text = "" + numberTurns [_numberTurnValue];
+
 	}
 }
