9cdb6b720623a6f0f2a9b6496b214bdfdedfc3bf
Ewakhine <wenell@hotmail.fr>
2015-05-26 18:50:40 +0200

    Begin of ConfigMenuManager.

---
+++ Assets/Scripts/MainMenu/ConfigMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ConfigMenuManager : MonoBehaviour {
+
+	public GameObject[] PanelList;
+
+	public string[] mapNames = {"EasyPlain", "GrassHills", "SpringLoop"};
+	public string[] vehiclesName;
+	public int[] numberPlayers = {1, 2, 3, 4};
+	public int[] numberVehicles = {2, 3, 4, 5, 6, 7 ,8};
+
+	void start() {
+		Debug.Log ("Piou");
+	}
+
+	void update() {
+		Debug.Log("piou");
+		for (int i= 0; i < PanelList.Length; i++) 
+		{
+			PanelList[i].GetComponentInChildren<Text>().text = "" + PanelList[i].GetComponentInChildren<Controler>().value;
+		}
+	}
+}
---
+++ Assets/Scripts/MainMenu/Controler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controler : MonoBehaviour {
+
+	public int value;
+	public int valueMax;
+
+	public void NextSelection() {
+		value += 1;
+		if (value > valueMax)
+			value = valueMax;
+	}
+	
+	public void PreviousSelection() {
+		value -= 1;
+		if (value < 0)
+			value = 0;
+	}
+}
