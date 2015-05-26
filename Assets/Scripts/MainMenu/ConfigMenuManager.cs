c09488e45812205b73d7c07425bb2e52e2872156
Ewakhine <wenell@hotmail.fr>
2015-05-26 21:09:05 +0200

    Update to DataRace.

--- Assets/Scripts/MainMenu/ConfigMenuManager.cs
+++ Assets/Scripts/MainMenu/ConfigMenuManager.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 
 public class ConfigMenuManager : MonoBehaviour {
 
+	public GameObject DataRace;
+
 	public RectTransform RaceControler;
 	public Image RaceChoiceImage;
 	public Sprite[] RaceImages;
@@ -33,19 +35,24 @@ public class ConfigMenuManager : MonoBehaviour {
 		int _raceValue = RaceControler.GetComponent<Controler>().value;
 		RaceChoiceImage.sprite = RaceImages [_raceValue];
 		RaceName.text = mapNames [_raceValue];
+		DataRace.GetComponent<DataRace> ().sceneToLoad = _raceValue + 2;
 		
 		int _vehicleValue = VehicleControler.GetComponent<Controler> ().value;
 		VehicleChoiceImage.sprite = VehicleImages [_vehicleValue];
 		VehicleName.text = vehicleNames [_vehicleValue];
+		DataRace.GetComponent<DataRace> ().vehiclePlayer = _vehicleValue;
 
-		int _playerValue = NumberPlayerControler.GetComponent<Controler> ().value;
-		CounterPlayers.text = "" + numberPlayers [_playerValue];
+		int _numberPlayerValue = NumberPlayerControler.GetComponent<Controler> ().value;
+		CounterPlayers.text = "" + numberPlayers [_numberPlayerValue];
+		DataRace.GetComponent<DataRace> ().numberPlayers = numberPlayers [_numberPlayerValue];
 		
 		int _numberVehicleValue = NumberVehicleControler.GetComponent<Controler> ().value;
 		CounterVehicles.text = "" + numberVehicles [_numberVehicleValue];
+		DataRace.GetComponent<DataRace> ().numberVehicles = numberVehicles [_numberVehicleValue];
 		
 		int _numberTurnValue = NumberTurnControler.GetComponent<Controler> ().value;
 		CounterTurn.text = "" + numberTurns [_numberTurnValue];
+		DataRace.GetComponent<DataRace> ().numberTurns = numberTurns [_numberTurnValue];
 
 	}
 }
--- Assets/Scripts/MainMenu/DataRace.cs
+++ Assets/Scripts/MainMenu/DataRace.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class DataRace : MonoBehaviour {
 
-	public int numberPlayers = 6;
+	public int numberPlayers = 1;
+	public int numberVehicles = 6;
+	public int numberTurns = 3;
 	public int sceneToLoad = 3;
-	public GameObject vehiclePlayer;
+	public int vehiclePlayer;
 }
