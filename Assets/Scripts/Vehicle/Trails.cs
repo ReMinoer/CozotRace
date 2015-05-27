c80596409bf6df5bb926bc95b11086bc8afb20e4
Valenitn <val.roucoux@free.fr>
2015-05-27 18:02:46 +0200

    Update Trails.

---
+++ Assets/Scripts/Vehicle/Trails.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trails : MonoBehaviour {
+	protected Vector3 velocity2D;
+	public float vit;
+	public float seuil;
+	// Use this for initialization
+	void Start () {
+		GetComponent<TrailRenderer> ().enabled = false;
+		vit = 0;
+		//seuil = 3.8f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		velocity2D = gameObject.GetComponentInParent<Rigidbody> ().velocity;
+		velocity2D.y = 0;
+		float speed = velocity2D.magnitude;
+		if (speed < seuil) {
+			GetComponent<TrailRenderer> ().enabled = false;
+		} else {
+			GetComponent<TrailRenderer> ().enabled = true;
+		}
+		vit = speed;
+
+	}
+}
